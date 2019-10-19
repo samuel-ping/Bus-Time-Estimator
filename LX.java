4587bb33bcea879a52e04dd0431cf01b38e7783f
Samuel P <55605862+samuel-ping@users.noreply.github.com>
2019-10-19 15:52:52 -0400

    Delete LX.java
    Instead of making new classes for each bus, we will make a new object for each bus in the App class.

--- LX.java
+++
@@ -1,7 +0,0 @@
-
-public class LX extends Bus {
-	public LX() {
-		Stop[] route = new Stop[] {new Stop("College Avenue Student Center", 5), new Stop("Scott Hall", 2), new Stop("Train Station (by Somerset St)", 3)}; 
-	}
-	
-}
