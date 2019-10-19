5d02cdb48119bd6b2a3000f8d01a42e2c39d579c
Samuel P <55605862+samuel-ping@users.noreply.github.com>
2019-10-19 15:30:54 -0400

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,32 @@
+
+public abstract class Bus {
+	protected Stop[] route;
+	
+	public Stop[] getRoute() {
+		return route;
+	}
+	
+	public int getTime(int index) {
+		return route[index].getTimeToNextStop();
+	}
+	
+	public boolean contains(String start, String end) {
+		for(int i = 0; i < route.length; i++) {
+			if(start.equals(route[i].getStopName()) || end.equals(route[i].getStopName())) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public int getStartingIndex(String name) {
+		int index = 0;
+		for(int i = 0; i < route.length; i++) {
+			if(name.equals(route[i].getStopName())) {
+				index = i;
+			}
+		}
+		return index;
+	}
+}
